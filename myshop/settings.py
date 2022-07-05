d3261870ea6a3c24f1625bafa7aea0f13c97b9ee
weshy <josephwaweru96@gmail.com>
2022-07-05 15:27:01 +0300

    order model

--- myshop/settings.py
+++ myshop/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
     'shop',
     'cart',
+    'orders',
 
 ]
 
--- myshop/urls.py
+++ myshop/urls.py
@@ -20,7 +20,10 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    
     path('cart/', include('cart.urls', namespace='cart')),
+    path('orders/', include('orders.urls', namespace='orders')),
+
     path('', include("shop.urls", namespace='shop')),
 ]
 
