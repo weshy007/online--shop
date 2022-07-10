5db1e7a0b7416be8dcc34eae73e4f54c4bec4d74
weshy <josephwaweru96@gmail.com>
2022-07-10 18:50:50 +0300

    having invoices rendered in PDF formart

--- orders/admin.py
+++ orders/admin.py
@@ -42,6 +42,13 @@ def order_detail(obj):
     url = reverse('orders:admin_order_detail', args=[obj.id])
     return mark_safe(f'<a href="{url}">View</a>')
 
+# Rendering PDF files
+def order_pdf(obj):
+    url = reverse('orders:admin_order_pdf', args=[obj.id])
+    return mark_safe(f'<a href="{url}">PDF</a>')
+
+order_pdf.short_description = 'Invoice'
+
 
 
 # Register your models here.
@@ -53,7 +60,7 @@ class OrderItemInline(admin.TabularInline):
 class OrderAdmin(admin.ModelAdmin):
     list_display = ['id', 'first_name', 'last_name', 'email',
                     'address', 'postal_code', 'city', 'paid',
-                    'created', 'updated', order_detail]
+                    'created', 'updated', order_detail, order_pdf]
     list_filter = ['paid', 'created', 'updated']
     inlines = [OrderItemInline]
 
